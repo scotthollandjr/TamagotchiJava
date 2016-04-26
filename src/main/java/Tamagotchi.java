a3c70e0155e971fe0cc28b8f1ec29f2cde911ea7
Jeffrey Hackford and Scott Holland <jeffrey@>
2016-04-26 15:01:28 -0700

    add Tamagotchi object and first few tests for the custom methods on the object.

--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -1,5 +1,32 @@
 import java.util.ArrayList;
 
 public class Tamagotchi {
+  private String mName;
+  private String mPoopCount;
+  private String mHungerLevel;
+  private String mEnergyLevel;
+  private String mHappinessLevel;
 
+  public Tamagotchi(String name, String poopCount, String hungerLevel, String energyLevel, String happinessLevel) {
+    mName = name;
+    mPoopCount = poopCount;
+    mHungerLevel = hungerLevel;
+    mEnergyLevel = energyLevel;
+    mHappinessLevel = happinessLevel;
+  }
+  public String getName() {
+    return mName;
+  }
+  public String getPoopCount() {
+    return mPoopCount;
+  }
+  public String getHungerLevel() {
+    return mHungerLevel;
+  }
+  public String getEnergyLevel() {
+    return mEnergyLevel;
+  }
+  public String getHappinessLevel() {
+    return mHappinessLevel;
+  }
 }
--- src/test/java/TamagotchiTest.java
+++ src/test/java/TamagotchiTest.java
@@ -4,5 +4,25 @@ import java.util.ArrayList;
 
 public class TamagotchiTest {
 
+  @Test
+  public void Tamagotchi_instantiatesWithDescription_String() {
+    Tamagotchi myTamagotchi = new Tamagotchi("Pepe", "0", "5", "6", "7");
+    assertEquals("Pepe", myTamagotchi.getName());
+  }
+  @Test
+  public void Tamagotchi_getPoopCount_String() {
+    Tamagotchi myTamagotchi = new Tamagotchi("Pepe", "0", "5", "6", "7");
+    assertEquals("0", myTamagotchi.getPoopCount());
+  }
+  @Test
+  public void Tamagotchi_getHungerLevel_String() {
+    Tamagotchi myTamagotchi = new Tamagotchi("Pepe", "0", "5", "6", "7");
+    assertEquals("5", myTamagotchi.getHungerLevel());
+  }
+  @Test
+  public void Tamagotchi_getEnergyLevel_String() {
+    Tamagotchi myTamagotchi = new Tamagotchi("Pepe", "0", "5", "6", "7");
+    assertEquals("6", myTamagotchi.getEnergyLevel());
+  }
 
 }
